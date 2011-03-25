e4c95e86e31eeab0af99cccc6560f62e4e0082e0
David Greaves <david@dgreaves.com>
2011-03-25 16:30:38 +0000

    Update setup.py and changelog

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 from setuptools import setup
 
 setup(name='python-boss-skynet',
-      version='0.1',
+      version='0.2',
       description='SkyNET for BOSS',
       author='David Greaves',
       author_email='david@dgreaves.com',
       url='http://meego.gitorious.org/meego-infrastructure-tools/python-boss-skynet',
       packages=['SkyNET',],
       requires=['RuoteAMQP',],
-      data_files=[('/usr/share/doc/python-boss-skynet',['example-participant','README',]),
+      data_files=[('/usr/share/doc/python-boss-skynet',['example-check-participant','example-notify-participant','minimal-participant','README',]),
                   ]
      )

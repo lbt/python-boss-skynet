3146827e5121edd04bc39d4b9f54a69c1e32cb1e
David Greaves <david.greaves@jolla.com>
2021-04-26 17:52:29 +0100

    Update packaging to pyproject.toml
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import setuptools
 
-setup(name='boss-skynet',
-      version='__VERSION__',
-      description='SkyNET for BOSS',
-      author='David Greaves',
-      author_email='david@dgreaves.com',
-      url='https://github.com/MeeGoIntegration/python-boss-skynet',
-      packages=['SkyNET'],
-      requires=['RuoteAMQP'],
-      scripts=['scripts/skynet', 'scripts/skynet_exo'],
-      data_files=[('/usr/share/doc/python3-boss-skynet',
-                   ['examples/example-check-participant',
-                    'examples/example-notify-participant',
-                    'examples/minimal-participant',
-                    'README', 'INSTALL']),
-                  ('/etc/skynet', ['conf/skynet.conf', 'conf/skynet.env'])
-                  ])
+# setup() now reads setup.cfg
+if __name__ == "__main__":
+    setuptools.setup()
